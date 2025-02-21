09319607896c1fcce8a510d1a82cc3b9f901c59f
Sasidhar Chintapalli <sasidhar@infoworks.io>
2025-02-21 15:39:39 +0530

    working version

--- backend/routers/responses/comments_res.py
+++ backend/routers/responses/comments_res.py
@@ -1,5 +1,4 @@
 from backend.models.comments_model import CommentBaseResponse
-
 from backend.routers.responses.posts_res import PostBaseResponse
 
 
--- backend/routers/responses/users_res.py
+++ backend/routers/responses/users_res.py
@@ -1,8 +1,8 @@
 from typing import List
 from backend.models.users_model import  UserBaseResponse
-from backend.models.posts_model import PostResponse
+from backend.models.posts_model import PostBaseResponse
 
 class UserFullResponse(UserBaseResponse):
-    posts: List["PostResponse"] = []
+    posts: List["PostBaseResponse"] = []
 
  
